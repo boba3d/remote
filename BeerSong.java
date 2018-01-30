c171c1bd8f0a9e7699a925bf7fc7a1ef1dba52dd
marsol DAman <Marsol Daman>
2018-01-29 19:18:36 -0500

    Added proper implementation of the java file

--- BeerSong.java
+++ BeerSong.java
@@ -1,9 +1,29 @@
 
+//Marsol Daman
+//CSI 3370
+//"See your world through my eyes: A universe so vast as to be immeasurable - incomprehensible even to your greatest minds."
+//-Alagalon the Observer
 
-public main(){
 
+
+
+public class BeerSong {
+
+    public static void main(String[] args) {
+        BeerSong();
+    }
+    public static void beerSong(){
+<<<<<<< HEAD
+=======
+
+        for(int i = 99; i>1; i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer\n"
+                    +"take one down"+", pass it around," + i + " bottles of beer on the wall");
+
+        }
+        int i = 1;
+            System.out.println(i+" bottle of beer on the wall, "+i+" bottle of beer\n"
+                    +"take one down"+", pass it around," + i + " no more bottles of beer on the wall");
+>>>>>>> implementation
+    }
 }
-public Ninety_Nine_Bottles_of_Beer(){
-}
-
-
